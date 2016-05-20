69b1d64202d14f02de8ebcb1cb4645d8db107977
Mateusz Wozniak <kontakt@mateuszwozniak.pl>
2016-05-20 12:27:22 +0200

    add test case

---
+++ README.md
@@ -0,0 +1,11 @@
+It looks like window.getComputedStyle() in headless Chrome on OSX works incorrectly. 
+
+All number values have commas instead of points, e.g:
+- matrix 'matrix(0.1, 0.2, 0.3, 0.4, 0.5, 0.6)' applied to el.style.transform is returned as 'matrix(0,1, 0,2, 0,3, 0,4, 0,5, 0,6)'
+- time '0.2s' assigned to el.style.transformDuration is returned as '0,2s'
+
+To see that simply run:
+`npm install`
+and 
+`npm test`
+
---
+++ tests.js
@@ -0,0 +1,36 @@
+describe('getComputedStyle()', function () {
+
+	var el;
+	var matrix = 'matrix(0.1, 0.2, 0.3, 0.4, 0.5, 0.6)';
+	var time = '0.2s';
+
+	before(function () {
+		el = document.createElement('div');
+		el.style.transform = 'matrix(0.1, 0.2, 0.3, 0.4, 0.5, 0.6)';
+		el.style.transitionDuration = time;
+		document.body.appendChild(el);
+	});
+
+	after(function () {
+		document.body.removeChild(el);
+	});
+
+	it('should return correct matrix', function () {
+		var computedMatrix = window.getComputedStyle(el)['transform'];
+		console.log('computed matrix:', computedMatrix);
+		if (computedMatrix !== matrix) {
+			throw new Error(computedMatrix + ' expected to be equal to ' + matrix);
+		}
+		//assert.equal(matrix, computedMatrix);
+	});
+
+	it('should return correct time', function () {
+		var computedTime = window.getComputedStyle(el)['transitionDuration'];
+		console.log('computed time:', computedTime);	
+		if (computedTime !== time) {
+			throw new Error(computedTime + ' expected to be equal to ' + time);
+		}
+		//expect(computedTime).to.equal(time);
+	});
+
+});
